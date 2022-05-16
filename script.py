e3c4b12c865d3286060c0ece135a99f941ae40dd
AhmadSiddiqi158 <55935738+AhmadSiddiqi158@users.noreply.github.com>
2022-05-16 17:59:25 -0400

    Add files via upload

--- script.py
+++ script.py
@@ -9,7 +9,7 @@ print("Incorrect AOV: $" + str(total_order_amount/rows))
 for row in df:
     df["Average_shoe_price"] = df["order_amount"]/df["total_items"]
 
-df = df.sort_values(["Average_shoe_price"], ascending=[False]) #ordering the data in a descending order of average shoe price (ASP)
+#df = df.sort_values(["Average_shoe_price"], ascending=[False]) #ordering the data in a descending order of average shoe price (ASP)
 df = df[df.Average_shoe_price < 1000] #assuming an Average shoe price costs less than $1000 ($1000 too is on the high end side). 
 #This way we filter out all the outliers (extremely expensive shoes) that skew up the data
 total_average_order = df["Average_shoe_price"].sum() # summing up all the ASP after filtering
